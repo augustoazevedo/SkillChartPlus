1000a1bc3790f33c34759d671201c0cf29507b60
Lala Azizli <122748161+lazizli@users.noreply.github.com>
2023-04-26 22:38:41 +0400

    Update levelinfo.tsx

--- SkillChart/levelinfo.tsx
+++ SkillChart/levelinfo.tsx
@@ -15,11 +15,6 @@ export const levelDescriptions = [
     description: "Data Analysis L3\n\nYou can use data to drive product decisions and can effectively analyze complex data sets.\n\nYou can communicate data insights to stakeholders in a clear and actionable manner."
   },
   {
-    skill: 'Data Analysis',
-    level: '4',
-    description: "Data Analysis L4\n\nYou are an expert in data analysis and can use data to drive business results.\n\nYou can analyze complex data sets and can effectively communicate data insights to stakeholders to drive informed decisions."
-  },
-  {
     skill: 'Problem solving',
     level: '1',
     description: "Problem Solving L1\n\nYou may be lacking analytical rigor in your reasoning and may have difficulty solving problems independently.\n\nYou may require a lot of guidance in problem-solving."
@@ -35,9 +30,79 @@ export const levelDescriptions = [
     description: "Problem Solving L3\n\nYou are able to use logic, insights, and prior knowledge to effectively define and structure problems.\n\nYou can synthesize findings from analyses and develop impactful recommendations.\n\nYou also have the ability to coach team members in problem-solving fundamentals."
   },
   {
-    skill: 'Problem solving',
-    level: '4',
-    description: "Problem Solving L4\n\nYou combine logic and creativity in problem-solving, along with relevant context to come up with innovative solutions and highly impactful recommendations.\n\nYou have the potential to improve the process for how your organization solves problems, and you can train and coach other team members on effective problem-solving techniques."
-  }
+    skill: 'Performing analysis',
+    level: '1',
+    description: "Performing analysis L1\n\nYou may sometimes find it challenging to read diagrams and draw conclusions from them.\n\n"
+  },
+  {
+    skill: 'Performing analysis',
+    level: '2',
+    description: "Performing analysis L2\n\nYou have the ability to understand diagrams and can find your own data based on our premade analysis using tools like Metabase. You are capable of defining analyses to answer your own questions and can conduct simple analyses in tools such as Fullstory, SQL, and MongoDB.\n\n"
+  },
+  {
+    skill: 'Performing analysis',
+    level: '3',
+    description: "Performing analysis L3\n\nYou can conduct more advanced analyses and have the skills to successfully coach others in improving their analytics abilities. You can also set up reports and data that is easily accessible to relevant stakeholders, and actively promote a data-driven mindset across the team.\n\n"
+  },
+  {
+    skill: 'Metrics identification',
+    level: '1',
+    description: "Metrics identification L1\n\nSometimes, you may struggle to define metrics for given product and need guidance. You may require specific direction on how to choose metrics that align with product goals.\n\n"
+  },
+  {
+    skill: 'Metrics identification',
+    level: '2',
+    description: "Metrics identification L2\n\nYou have a good understanding of metrics identification and can select relevant metrics that align with product objectives. You have the ability to clearly discuss the pros and cons of various metrics, and make a solid argument for the choice(s) that you select.\n\n"
+  },
+  {
+    skill: 'Metrics identification',
+    level: '3',
+    description: "Metrics identification L3\n\nYou develop a deep understanding of the product's user journey and identify key touchpoints where metrics can be used to measure success and drive improvement. You are able to create or select the most appropriate metrics framework based on the product's stage of development, business objectives, and other contextual factors. You thoroughly and accurately discuss metrics, and can even recommend ways to streamline analytical processes for maximum efficiency.\n\n"
+  },
+  {
+    skill: 'Metrics prioritization',
+    level: '1',
+    description: "Metrics prioritization L1\n\nSometimes, you may struggle with prioritization and need guidance.\n\n"
+  },
+  {
+    skill: 'Metrics prioritization',
+    level: '2',
+    description: "Metrics prioritization L2\n\nYou ensure that you prioritize effectively, taking into account factors such as urgency, cost/benefit, and testability.\n\n"
+  },
+  {
+    skill: 'Metrics prioritization',
+    level: '3',
+    description: "Metrics prioritization L3\n\nYou have the ability to balance conflicting priorities and trade-offs, track and analyze KPIs, and use data-driven decision-making to optimize metrics over time. Your leadership skills and experience enable you to coach and mentor others while facilitating collaboration among cross-functional teams to achieve shared goals and outcomes.\n\n"
+  },
+  {
+    skill: 'Prioritization',
+    level: '1',
+    description: "Prioritization L1\n\nYou may have a limited understanding of prioritization, delivery, and optimization practices, which can lead to avoidable mishaps and blockages in the process.\n\n"
+ },
+{
+    skill: 'Prioritization',
+    level: '2',
+    description: "Prioritization L2\n\nYYou generally and reasonably prioritize customer outcomes and business goals in relation to your own and your team's efforts. You understand and effectively play a part in the delivery process, while also ensuring that team members can independently work on tasks assigned to them. You limit work in progress and ensure flow.\n\n"
+ },
+{
+    skill: 'Prioritization',
+    level: '3',
+    description: "Prioritization L3\n\nYou work with your team to prioritize opportunities and solutions, and get buy-in from stakeholders. You take responsibility for the process, with clear resulting tasks and proper handovers to product marketing, customer support, and analytics. Your team can work with focus, and you identify and act upon improvement potential.\n\n"
+ },
+  {
+    skill: 'Visual (UI)',
+    level: '1',
+    description: "Visual (UI) L1\n\nYou may have a limited understanding of composition, color theory, and typography. You may struggle to detect inconsistencies in designs and branding assets. You are not aware of industry patterns and trends \n\n"
+ },
+{
+    skill: 'Visual (UI)',
+    level: '2',
+    description: "Visual (UI) L2\n\nYou have a good understanding of composition, color theory, typography, and design principles. You can identify inconsistencies and suggest improvements to your product designs. You are up to date on industry patterns and trends and can make informed decisions about design choices.\n\n"
+ },
+{
+    skill: 'Visual (UI)',
+    level: '3',
+    description: "Visual (UI) L3\n\nYou possess a deep understanding of composition, color theory, typography, and design principles, and can use this knowledge to provide guidance and mentorship to junior team members. You have a strong sense of the brand and can ensure that all design work is consistent with the brand identity. Additionally, you stay up to date on industry trends and can identify opportunities to innovate and improve the visual design of the product.\n\n"
+ }
 
 ];
